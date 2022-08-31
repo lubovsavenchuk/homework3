548c508fef451f313ea7fb5bf838a8137fc23037
Савенчук Любовь <sav_lu@gmail.com>
2022-08-31 20:57:10 +0500

    ex1/

---
+++ ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
+
+Console.Write("Type any number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] cubes = new int [N];
+int index = 0;
+int num = 1;
+
+while (index < N)
+{
+   cubes[index] = num * num * num;
+   Console.Write(cubes[index] + " ");
+   num ++;
+   index ++;
+}
